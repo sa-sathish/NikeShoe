18b60b2341b34f934453036111e71a8d90d3b2a2
Sathish_Work <sa.shanmugam@tawari.sa>
2021-04-18 21:17:25 +0300

    Initial Commit

---
+++ NIkeShoe/NIkeShoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIkeShoeApp.swift
+//  NIkeShoe
+//
+//  Created by sathish s on 18/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct NIkeShoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
